0592331a144bd0ecdd6fbc74f6aaa7df7a581d71
raygie <raygieramos18@gmail.com>
2022-04-19 11:36:26 +0800

    update add function

--- Admin/allproducts.php
+++ Admin/allproducts.php
@@ -74,16 +74,6 @@ $a=7;
         <div class="card">
             <div class="card-body table-responsive">
                 <h5 class="card-title">Datatables</h5>
-                <?php
-                  if(isset($_SESSION['success']) && $_SESSION['success'] !=''){
-                    echo '<h2 class="bg-primary text-white"> '.$_SESSION['success'].'</h2>';
-                    unset($_SESSION['success']);
-                  }
-                  if(isset($_SESSION['status']) && $_SESSION['status'] !=''){
-                    echo '<h2 class="bg-danger text-white"> '.$_SESSION['status'].'</h2>';
-                    unset($_SESSION['status']);
-                  }
-                ?>
                 <table class="table datatable">
                     <thead>
                         <tr>
--- Admin/includes/addproduct.php
+++ Admin/includes/addproduct.php
@@ -1,7 +1,7 @@
 <?php  
 require '../includes/connection.php';
 if(isset($_POST['submit'])){
-    $prod_id = uniqid();
+    
     $inputprod_name = $_POST['inputprod_name'];
     $inputprod_price= $_POST['inputprod_price'];
     $inputprod_link = $_POST['inputprod_link'];
@@ -9,23 +9,16 @@ if(isset($_POST['submit'])){
     $inputprod_desc = $_POST['inputprod_desc'];
     $prod_image = $_FILES["prod_image"]['name'];
 
-    if(file_exists("prodpic/".$_FILES["prod_image"]["name"])){
-        $store = $_FILES["prod_image"]["name"];
-        $_SESSION['status'] = "Image Already Exists. '.$store.'";
-        header('Location: ../allproducts.php');
-    }
-    else{
-        $sql = "INSERT INTO products (id,prod_name,prod_price,link,prod_category,prod_desc,prod_image) values('$prod','$inputprod_name','$inputprod_price','$inputprod_link','$inputprod_category','$inputprod_desc','$prod_image')";
+    
+        $sql = "INSERT INTO products (prod_name,prod_price,link,prod_category,prod_desc,prod_image) values('$inputprod_name','$inputprod_price','$inputprod_link','$inputprod_category','$inputprod_desc','$prod_image')";
         
         if(mysqli_query($conn, $sql)){
             move_uploaded_file($_FILES["prod_image"]["tmp_name"], "prodpic/".$_FILES["prod_image"]["name"]);
-            $_SESSION['success'] = "Product Added";
             header('Location: ../allproducts.php');
         }
         else{
-            $_SESSION['success'] = "Product Not added";
             echo "Error : ".$conn->error;
         }
-    }
+    
 }
 ?>
